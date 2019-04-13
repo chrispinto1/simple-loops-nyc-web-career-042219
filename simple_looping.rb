f043d46936a79923d32f1695e77d1f273a6d5747
Christian Pinto <pintochristian2@gmail.com>
2019-04-13 21:43:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     puts phrase
-    if number_of_times 
+    number_of_times += 1
+    break if number_of_times >= 7
 end
 
 def times_iterator(number_of_times)
